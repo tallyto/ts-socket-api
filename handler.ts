1ca1b76c9277460974c6fd4773babc3b63e35234
otallyto <rodrigues.tallyto@hotmail.com>
2020-12-18 10:08:57 -0400

    fix: handler error

--- handler.ts
+++ handler.ts
@@ -3,7 +3,7 @@ import 'source-map-support/register';
 var AWS = require('aws-sdk');
 var sqs = new AWS.SQS({ apiVersion: '2012-11-05' });
 AWS.config.update({ region: 'us-east-1' });
-export const hello: APIGatewayProxyHandler = async (event, _context) => {  
+export const hello: APIGatewayProxyHandler = async (event, _context) => {    
   try {  
     const{ body, requestContext: {connectionId}} = event;
 
@@ -22,15 +22,20 @@ export const hello: APIGatewayProxyHandler = async (event, _context) => {
       QueueUrl: "https://sqs.us-east-1.amazonaws.com/149239330696/socket-fila"
     }
 
-
     await sqs.sendMessage(sqsMessage).promise()
     return {
       statusCode: 200,
       body: JSON.stringify({
-        message: `${event.body}`,
+        message: body,
       }, null, 2),
     };
   } catch (error) {
     console.log("fucking error", error)
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: error,
+      }, null, 2),
+    };
   }
 }
