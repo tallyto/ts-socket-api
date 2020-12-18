6dc0dddf4a382ed6d7e7f88f760aed99a19969ff
otallyto <rodrigues.tallyto@hotmail.com>
2020-12-18 06:35:28 -0400

    feat: send message lambda socket

---
+++ serverless.ts
@@ -0,0 +1,47 @@
+import type { AWS } from '@serverless/typescript';
+
+const serverlessConfiguration: AWS = {
+  service: 'ts-socket-api',
+  frameworkVersion: '2',
+  custom: {
+    webpack: {
+      webpackConfig: './webpack.config.js',
+      includeModules: true
+    }
+  },
+  // Add the serverless-webpack plugin
+  plugins: ['serverless-webpack'],
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs12.x',
+    apiGateway: {
+      minimumCompressionSize: 1024,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+    },
+  },
+  functions: {
+    hello: {
+      handler: 'handler.hello',
+      events: [
+        {
+          websocket: {
+            route: "$connect",
+          },
+        }, {
+          websocket: {
+            route: "$disconnect",
+          },
+        },
+        {
+          websocket: {
+            route: "$default",
+          },
+        },
+      ]
+    }
+  }
+}
+
+module.exports = serverlessConfiguration;
